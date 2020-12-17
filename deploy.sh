73112729b410cc5f50912df7dbef7fd2c6335af1
Neil Farrington <me@neilfarrington.com>
2020-12-17 23:50:01 +0000

    Deploy via GitHub Actions

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-aws s3 cp . s3://neilfarrington.com/ --recursive --exclude "*" --include "*.html"
-aws s3api put-bucket-website --bucket neilfarrington.com --website-configuration file://site.json
-aws s3api put-bucket-website --bucket www.neilfarrington.com --website-configuration file://www-redirect.json
